00dad22173a15c1fcd190d7cf02749adc3853a80
arnavjainn06 <jainarnav@outlook.com>
2021-09-04 13:24:46 +0530

    uploaded new assets

--- main.py
+++ main.py
@@ -3,3 +3,66 @@
 @author: Arnav Jain
 @dateOfCreation: 03/09/2021
 """
+import os
+import cv2
+import numpy as np
+import face_recognition
+
+print('Loading modules...')
+
+path = 'assets'
+images = []
+classNames = []
+myList = os.listdir(path)
+
+for each in myList:
+    newImg = cv2.imread(f'{path}/{each}')
+    images.append(newImg)
+    classNames.append(os.path.splitext(each)[0])
+
+def findEncodings(images):
+    encodedImages = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encoded = face_recognition.face_encodings(img)[0]
+        encodedImages.append(encoded)
+
+    return encodedImages
+
+
+print('Running encoding function')
+encodedImages = findEncodings(images)
+print('Encoding complete')
+
+print('Returning webcam parameters')
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, imgL = cap.read()
+    img = cv2.resize(imgL, (0, 0), None, 0.25, 0.25)
+    img = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(img)
+    encodesCurFrame = face_recognition.face_encodings(img, facesCurFrame)
+
+    for encodeFace, faceLocation in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodedImages, encodeFace)
+
+        faceDis = face_recognition.face_distance(encodedImages, encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex]
+
+
+# # Loading assets[0]
+# print('Loading Assets...')
+# epo1 = face_recognition.load_image_file("assets/epo1.jpg")
+# epo1 = cv2.cvtColor(epo1, cv2.COLOR_BGR2RGB)
+#
+# # Loading assets[1]
+# print('Coloring the assets')
+# epo2 = face_recognition.load_image_file("assets/epo2.jpg")
+# epo2 = cv2.cvtColor(epo2, cv2.COLOR_BGR2RGB)
+#
--- testcase.py
+++ testcase.py
@@ -2,18 +2,32 @@ import cv2
 import numpy as np
 import face_recognition
 
+print('Loading modules...')
+
 # Loading assets[0]
+print('Loading Assets...')
 epo1 = face_recognition.load_image_file("assets/epo1.jpg")
 epo1 = cv2.cvtColor(epo1, cv2.COLOR_BGR2RGB)
 
 # Loading assets[1]
+print('Coloring the assets')
 epo2 = face_recognition.load_image_file("assets/epo2.jpg")
 epo2 = cv2.cvtColor(epo2, cv2.COLOR_BGR2RGB)
 
 # Reading face light points and locating face
+print('Encoding and loading face')
 epo1Location = face_recognition.face_locations(epo1)[0]
 encoded_epo1 = face_recognition.face_encodings(epo1)[0]
 cv2.rectangle(epo1, (epo1Location[3], epo1Location[0]), (epo1Location[1], epo1Location[2]), (0, 155, 0), 2)
 
-cv2.imshow("HorizonV4", epo1)
+epo2Location = face_recognition.face_locations(epo2)[0]
+encoded_epo2 = face_recognition.face_encodings(epo2)[0]
+cv2.rectangle(epo2, (epo2Location[3], epo2Location[0]), (epo2Location[1], epo2Location[2]), (0, 155, 0), 2)
+
+result = face_recognition.compare_faces([encoded_epo1], encoded_epo2)[0]
+distance = face_recognition.face_distance([encoded_epo1], encoded_epo2)
+
+print('Initializing Module Presentation')
+cv2.imshow("HorizonV4 - EPO1", epo1)
+cv2.imshow("HorizonV4 - EPO2", epo2)
 cv2.waitKey(0)
